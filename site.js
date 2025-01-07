5d33b3a39509149a1c5ddd78db869b8ddad4e8b1
525gtarantola <144464759+525gtarantola@users.noreply.github.com>
2025-01-07 16:19:14 +0000

    posterClick and timeText methods, customized all movies instead of 3

--- site.js
+++ site.js
@@ -26,24 +26,84 @@
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
-      created () {
+      created() {
             fetch('movies.json').then(response => response.json()).then(json => {
                   this.movies = json
             })
       },
       data() {
-        return {
-            title: "IMDB + Gabby's Top 8 Movies",
-            owner: "Gabrielle Tarantola",
-            github: "https://github.com/525gtarantola",
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
-    },
+            return {
+                  title: "IMDB + Gabby's Top 8 Movies",
+                  owner: "Gabrielle Tarantola",
+                  github: "https://github.com/525gtarantola",
+                  // This holds your movies.json data.
+                  movies: [],
+                  /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+
+            }
+      },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dateArray) {
+
+                  let year = dateArray[0];
+                  let day = dateArray[2];
+                  switch (dateArray[1]) {
+                        case 1:
+                              month = "January";
+                              break;
+                        case 2:
+                              month = "February";
+                              break;
+                        case 3:
+                              month = "March";
+                              break;
+                        case 4:
+                              month = "April";
+                              break;
+                        case 5:
+                              month = "May";
+                              break;
+                        case 6:
+                              month = "June";
+                              break;
+                        case 7:
+                              month = "July";
+                              break;
+                        case 8:
+                              month = "August";
+                              break;
+                        case 9:
+                              month = "September";
+                              break;
+                        case 10:
+                              month = "October";
+                              break;
+                        case 11:
+                              month = "November";
+                              break;
+                        case 12:
+                              month = "December";
+                              break;
+                  }
+                  return `${month} ${day}, ${year}`
+                  
+            },
+            posterClick(index) {
+
+                  if(this.movies[index].posterindex > this.movies[index].posters.length - 2) {
+                        this.movies[index].posterindex = 0;
+                  }else{
+                        this.movies[index].posterindex++
+                  }
+
+            },
+            timeText(minutes) {
+
+                  let hrs = Math.floor(minutes / 60);
+                  let mins = minutes % 60;
+                  return `${hrs}h ${mins}m`
+            }
       }
 })
 
